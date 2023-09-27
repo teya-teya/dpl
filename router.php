26a15c8d802e7d3d5489dbc163d566357baf4f0f
teya-teya <teya.teya89@yandex.ru>
2023-09-27 10:18:42 +0300

    add .htacces and routing

---
+++ router.php
@@ -0,0 +1,26 @@
+<?php
+$url = explode("/", $_SERVER['REQUEST_URI']);
+
+if ($url[1] == "v_asaeva") {
+  $content = file_get_contents("pages/index.html");
+} else if ($url[1] == "gallery") {
+  $content = file_get_contents("pages/gallery.html");
+} else if ($url[1] == "black&white") {
+  $content = file_get_contents("pages/blackwhite.html");
+} else if ($url[1] == "colour") {
+  $content = file_get_contents("pages/colour.html");
+} else if ($url[1] == "public") {
+  $content = file_get_contents("pages/public.html");
+} else if ($url[1] == "services") {
+  $content = file_get_contents("pages/services.html");
+} else if ($url[1] == "reviews") {
+  $content = file_get_contents("pages/reviews.html");
+} else if ($url[1] == "blog") {
+  $content = file_get_contents("pages/blog.html");
+} else if ($url[1] == "contacts") {
+  $content = file_get_contents("pages/contacts.html");
+} else {
+  $content = file_get_contents("pages/index.html");
+}
+if (!empty($content))
+  require_once("template.php");
